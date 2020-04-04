91b155575f6513d066e8d8feeffe8f27bdf408c4
carlaulloa <carla.ulloa.lp@gmail.com>
2020-04-04 13:57:52 -0500

    feat:base seguridad

---
+++ src/test/java/com/app/aforo255/seguridad/ServucesBankaforo255SeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServucesBankaforo255SeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
