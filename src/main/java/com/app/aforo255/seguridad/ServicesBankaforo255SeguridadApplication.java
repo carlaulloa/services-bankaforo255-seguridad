f893719637c5b0239413aa959f7b0e10b00d92ce
carlaulloa <carla.ulloa.lp@gmail.com>
2020-04-11 16:53:32 -0500

    feat: adicion mysql

--- src/main/java/com/app/aforo255/seguridad/ServicesBankaforo255SeguridadApplication.java
+++ src/main/java/com/app/aforo255/seguridad/ServicesBankaforo255SeguridadApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServucesBankaforo255SeguridadApplication {
+public class ServicesBankaforo255SeguridadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServucesBankaforo255SeguridadApplication.class, args);
+		SpringApplication.run(ServicesBankaforo255SeguridadApplication.class, args);
 	}
 
 }
--- src/test/java/com/app/aforo255/seguridad/ServicesBankaforo255SeguridadApplicationTests.java
+++ src/test/java/com/app/aforo255/seguridad/ServicesBankaforo255SeguridadApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServucesBankaforo255SeguridadApplicationTests {
+class ServicesBankaforo255SeguridadApplicationTests {
 
 	@Test
 	void contextLoads() {
